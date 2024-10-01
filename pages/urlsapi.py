2a0c36e667363e58bb30178afc39991bbf49e8bd
henriquepadua <79533064+henriquepadua@users.noreply.github.com>
2024-10-01 16:52:04 -0300

    Update urlsapi.py

--- pages/urlsapi.py
+++ pages/urlsapi.py
@@ -3,28 +3,20 @@ from django.db import router
 from django.urls import include, path
 from pages import views
 from rest_framework.routers import DefaultRouter
-from pages.views import AuthorViewSet, BookInstanceViewSet, BookViewSet, CustomTokenObtainPairView, GenreViewSet, LoanedBooksByUserListViewSet, MyObtainTokenPairView
-
-from rest_framework import routers, serializers, viewsets
-
-from pages.views import UserViewSet
-
-# Routers provide a way of automatically determining the URL conf.
-# router = routers.DefaultRouter()
-# router.register(r'users', UserViewSet)
+from pages.views import AuthorViewSet, BookInstanceViewSet, BookViewSet, GenreViewSet, LoanedBooksByUserListViewSet, MyObtainTokenPairView
 
 app_name = 'api'
-# router = routers.DefaultRouter()
-# router.register(r'users', UserViewSet)
 
 router = DefaultRouter(trailing_slash=False)
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
 router.register(r'books', BookViewSet)
 router.register(r'genre', GenreViewSet)
 router.register(r'author', AuthorViewSet)
 router.register(r'bookinstance', BookInstanceViewSet)
+#router.register(r'login', MyObtainTokenPairView, basename='login')
+
+# router.register(r'my-borrowed', LoanedBooksByUserListViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('login/', MyObtainTokenPairView.as_view(), name='login'),  # Definindo a rota para login diretamente
 ]
