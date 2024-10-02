addde4bf25d8e3ad8a5ef6d07546e3b08a7f0e02
henriquepadua <79533064+henriquepadua@users.noreply.github.com>
2024-10-02 17:01:56 -0300

    Update settings.py

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -143,3 +143,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 CORS_ALLOW_ALL_ORIGINS = True
 #CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:8000', 'http://localhost:8000', 'http://localhost:5173', 'http://127.0.0.1:5173']
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+}
