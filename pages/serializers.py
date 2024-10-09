14d1c5ae9374e47e2d03b901de3efa748cb2b861
henriquepadua <79533064+henriquepadua@users.noreply.github.com>
2024-10-09 17:06:41 -0300

    Update serializers.py

--- pages/serializers.py
+++ pages/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
 from pages.models import Author, Book, BookInstance, Genre
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-
     @classmethod
     def get_token(cls, user):
         token = super(MyTokenObtainPairSerializer, cls).get_token(user)
@@ -15,26 +15,36 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         return token
 
 class BookInstanceSerializer(serializers.ModelSerializer):
+  #book = serializers.StringRelatedField()#serializers.PrimaryKeyRelatedField(many=True, queryset=Book.objects.all())
   class Meta:
     model = BookInstance
     fields = '__all__'
 
 class BookSerializer(serializers.ModelSerializer):
   genre = serializers.PrimaryKeyRelatedField(many=True, queryset=Genre.objects.all())
-  bookinstance_set = BookInstanceSerializer(many=True,required=False)
-
+  bookinstance_set = serializers.SerializerMethodField()
+    
   class Meta:
       model = Book
       fields = '__all__'
 
-  def create(self, validated_data):
-      bookinstances_data = validated_data.pop('bookinstance_set', [])
-      genres = validated_data.pop('genre')
-      book = Book.objects.create(**validated_data)
-      book.genre.set(genres)  # Use set to assign many-to-many field
-      for bookinstance_data in bookinstances_data:
-            BookInstance.objects.create(book=book, **bookinstance_data)
-      return book
+  def get_bookinstance_set(self, obj):
+        # Acessa o request do contexto do serializer
+        request = self.context.get('request')
+        
+        # Filtra as instâncias de livros que pertencem ao usuário logado
+        book_instances = BookInstance.objects.filter(book=obj, borrower=request.user)
+
+        # Retorna apenas os IDs das instâncias
+        return BookInstanceSerializer(book_instances, many=True).data
+  # def create(self, validated_data):
+  #     bookinstances_data = validated_data.pop('bookinstance_set', [])
+  #     genres = validated_data.pop('genre')
+  #     book = Book.objects.create(**validated_data)
+  #     book.genre.set(genres)  # Use set to assign many-to-many field
+  #     for bookinstance_data in bookinstances_data:
+  #           BookInstance.objects.create(book=book, **bookinstance_data)
+  #     return book
   # bookinstance_set = BookInstanceSerializer(many=True,required=False)
   # class Meta:
   #   model = Book
@@ -52,6 +62,12 @@ class BookSerializer(serializers.ModelSerializer):
   #       BookInstance.objects.create(book=book, **book_instance)
 
   #   return book            
+
+class UserSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    fields = '__all__'
+        
     
 class AuthorSerializer(serializers.ModelSerializer):
   class Meta:
